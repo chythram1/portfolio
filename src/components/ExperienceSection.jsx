80ea03e2c6b3eb60054658f17023b28a1a42adb2
chythram1 <chythram@vt.edu>
2025-08-09 18:00:01 -0400

    Added experience and fixed location of index.html

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -1,6 +1,6 @@
 import { ArrowRight, ExternalLink, Github } from "lucide-react";
 
-const projects = [
+const experience = [
   {
     id: 1,
     title: "Software Engineering Intern at Markel",
@@ -13,7 +13,7 @@ const projects = [
     id: 2,
     title: "IT Intern at Indivior",
     description:
-      "Interactive analytics dashboard with data visualization and filtering capabilities.",
+      "Resolved technical issues for business users through ServiceNow, refined application documentation, and developed Power BI dashboard to visualize regional sales data for business teams",
     image: "/projects/experience2.gif",
     tags: ["Power BI", "ServiceNow", "Azure AD", "Adobe Premire Pro", "Excel"],
     
@@ -22,7 +22,7 @@ const projects = [
     id: 3,
     title: "Research Assistant at the Intrusion Detection Lab (VCU)",
     description:
-      "Research Project aimed at understanding conspiracy theory propagation networks on Twitter",
+      "Authored Research Paper with research team on our findings on conspiracy theory propagation networks on Twitter",
     image: "/projects/experience3.png",
     tags: ["Python", "Twitter", "Tweepy", "OverLeaf", "LaTeX", "Data Analysis"],
     demoUrl: "https://ieeexplore.ieee.org/document/10154285",
@@ -32,7 +32,7 @@ const projects = [
 
 export const ExperienceSection = () => {
   return (
-    <section id="projects" className="py-24 px-4 relative">
+    <section id="experience" className="py-24 px-4 relative">
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
           {" "}
@@ -44,31 +44,31 @@ export const ExperienceSection = () => {
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {projects.map((project, key) => (
+          {experience.map((experience, key) => (
             <div
               key={key}
               className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
             >
               <div className="h-48 overflow-hidden">
                 <img
-                  src={project.image}
-                  alt={project.title}
+                  src={experience.image}
+                  alt={experience.title}
                   className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-110"
                 />
               </div>
 
               <div className="p-6">
                 <div className="flex flex-wrap gap-2 mb-4">
-                  {project.tags.map((tag) => (
+                  {experience.tags.map((tag) => (
                     <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
                       {tag}
                     </span>
                   ))}
                 </div>
 
-                <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
+                <h3 className="text-xl font-semibold mb-1"> {experience.title}</h3>
                 <p className="text-muted-foreground text-sm mb-4">
-                  {project.description}
+                  {experience.description}
                 </p>
                 <div className="flex justify-between items-center">
                   <div className="flex space-x-3">
